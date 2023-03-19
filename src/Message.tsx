f9e61308684ae2fa0a12be79b3643cf7e3663c51
Roller Lo <jasonbh02@gmail.com>
2023-03-19 22:52:27 +0800

    feat(Message): replace div with h1 element

--- src/Message.tsx
+++ src/Message.tsx
@@ -4,8 +4,8 @@ function Message() {
     const name = 'Mosh';
 
     if (name)
-        return <div>Hello {name}</div>
-    return <div>Hello World</div>
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
 }
 
 export default Message;
