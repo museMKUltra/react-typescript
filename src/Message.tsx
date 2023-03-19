4059356dab43228c3b42a757fdda0c4e50542f85
Roller Lo <jasonbh02@gmail.com>
2023-03-19 22:49:22 +0800

    feat(Message): create first react component

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Mosh';
+
+    if (name)
+        return <div>Hello {name}</div>
+    return <div>Hello World</div>
+}
+
+export default Message;
