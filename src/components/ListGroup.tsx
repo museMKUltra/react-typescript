e2208463842d6f8c7d5667480de8a996fab16873
Roller Lo <jasonbh02@gmail.com>
2023-04-04 02:58:33 +0800

    feat(ListGroup): set selected index to active item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,23 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, useState } from 'react'
 function ListGroup() {
 	const items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris']
-
-	// Event handler
-	const handleClick = (event: MouseEvent) => console.log(event)
+	const [selectedIndex, setSelectedIndex] = useState(-1)
 
 	return (
 		<>
 			<h1>List</h1>
 			{items.length === 0 && <p>No item found</p>}
 			<ul className="list-group">
-				{items.map(item => (
-					<li className="list-group-item" key={item} onClick={handleClick}>
+				{items.map((item, index) => (
+					<li
+						className={
+							selectedIndex === index
+								? 'list-group-item active'
+								: 'list-group-item'
+						}
+						key={item}
+						onClick={() => setSelectedIndex(index)}
+					>
 						{item}
 					</li>
 				))}
