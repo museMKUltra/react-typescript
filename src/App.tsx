9e64023ab8efca8d3bf98333fd70271068946743
Roller Lo <jasonbh02@gmail.com>
2023-04-04 03:29:54 +0800

    feat(Alert): passing children in ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import ListGroup from './components/ListGroup'
+import Alert from './components/Alert'
 
 function App() {
-	const items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris']
-	const handleSelectItem = (item: string) => {
-		console.log(item)
-	}
-
 	return (
-		<div>
-			<ListGroup
-				items={items}
-				heading="Cities"
-				onSelectItem={handleSelectItem}
-			/>
-		</div>
+		<Alert>
+			Hello <span>World</span>
+		</Alert>
 	)
 }
 
