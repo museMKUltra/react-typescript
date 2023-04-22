70311a65a5c3f4d56ce6b3b9cb35144df7352ed9
Roller Lo <jasonbh02@gmail.com>
2023-04-22 14:14:11 +0800

    feat: dismissible alert to open & close with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import Alert from './components/Alert'
+import Button from './components/Button'
+import { useState } from 'react'
 
 function App() {
+	const [isAlertVisible, setIsAlertVisible] = useState(false)
+
 	return (
-		<Alert>
-			Hello <span>World</span>
-		</Alert>
+		<>
+			{isAlertVisible && (
+				<Alert onClose={() => setIsAlertVisible(false)}>
+					Hello <span>World</span>
+				</Alert>
+			)}
+			<Button onClick={() => setIsAlertVisible(true)}>button</Button>
+		</>
 	)
 }
 
