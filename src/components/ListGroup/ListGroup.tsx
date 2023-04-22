3e36c4c155bacfed22f14200c646b9a59086db07
Roller Lo <jasonbh02@gmail.com>
2023-04-22 16:16:06 +0800

    feat(ListGroup): use css-in-js style

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
-import styles from './ListGroup.module.css'
+import styled from 'styled-components'
+
+const List = styled.ul`
+	list-style: none;
+	padding: 0;
+`
+
+interface ListItemProps {
+	active: boolean
+}
+
+const ListItem = styled.li<ListItemProps>`
+	padding: 5px 0;
+	background: ${props => (props.active ? 'blue' : 'none')};
+`
 
 interface Props {
 	items: string[]
@@ -14,14 +28,10 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
 		<>
 			<h1>{heading}</h1>
 			{items.length === 0 && <p>No item found</p>}
-			<ul className={[styles.listGroup, styles.container].join(' ')}>
+			<List>
 				{items.map((item, index) => (
-					<li
-						className={
-							selectedIndex === index
-								? 'list-group-item active'
-								: 'list-group-item'
-						}
+					<ListItem
+						active={selectedIndex === index}
 						key={item}
 						onClick={() => {
 							setSelectedIndex(index)
@@ -29,9 +39,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
 						}}
 					>
 						{item}
-					</li>
+					</ListItem>
 				))}
-			</ul>
+			</List>
 		</>
 	)
 }
