dc1e667f763cd8142592bf81d58fdc6086f1c6b1
Roller Lo <jasonbh02@gmail.com>
2023-04-23 15:48:03 +0800

    feat(App): example for updating state by immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { BsAlarmFill } from 'react-icons/bs'
-import ListGroup from './components/ListGroup'
-import Button from './components/Button'
 import './App.css'
-import Like from './components/Like'
+import { useState } from 'react'
+import produce from 'immer'
 
 function App() {
+	const [bugs, setBugs] = useState([
+		{ id: 1, title: 'bug 1', fixed: false },
+		{ id: 2, title: 'bug 2', fixed: false },
+	])
+
+	const handleClick = () => {
+		setBugs(
+			produce(draft => {
+				const bug = draft.find(bug => bug.id === 1)
+				if (bug) bug.fixed = true
+			})
+		)
+	}
+
 	return (
 		<>
-			<Button onClick={() => console.log('ok')}>button</Button>
-			<ListGroup
-				items={['one', 'two', 'three']}
-				heading={'Number'}
-				onSelectItem={() => console.log('ok')}
-			/>
-			<div>
-				<BsAlarmFill />
-			</div>
-			<div>
-				<Like onClick={() => console.log('like')} />
-			</div>
+			{bugs.map(bug => (
+				<p key={bug.id}>
+					{' '}
+					{bug.title} {bug.fixed ? 'Fixed' : 'New'}{' '}
+				</p>
+			))}
+			<button onClick={handleClick}>click me</button>
 		</>
 	)
 }
