60b9d391478f8a9651d6164708727eb387143aba
Roller Lo <jasonbh02@gmail.com>
2023-05-06 15:27:28 +0800

    feat(Form): use react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,20 @@
-import { FormEvent, useRef, useState } from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
 
 function Form() {
-	const [person, setPerson] = useState({ name: '', age: '' })
+	const { register, handleSubmit } = useForm()
 
-	function handleSubmit(event: FormEvent) {
-		event.preventDefault()
-		console.log(person)
+	function onSubmit(data: FieldValues) {
+		console.log(data)
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="mb-3">
 				<label htmlFor="name" className="form-label">
 					Name
 				</label>
 				<input
-					value={person.name}
-					onChange={event => setPerson({ ...person, name: event.target.value })}
+					{...register('name')}
 					id="name"
 					type="text"
 					className="form-control"
@@ -27,10 +25,7 @@ function Form() {
 					Age
 				</label>
 				<input
-					value={person.age}
-					onChange={event =>
-						setPerson({ ...person, age: parseInt(event.target.value) })
-					}
+					{...register('age')}
 					id="age"
 					type="number"
 					className="form-control"
