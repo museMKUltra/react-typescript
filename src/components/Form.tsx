47db120ef377156d9f858573b0845b9cdd773f60
Roller Lo <jasonbh02@gmail.com>
2023-05-06 15:18:22 +0800

    feat(Form): useState & onChange handle input value

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,10 @@
-import { FormEvent, useRef } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 
 function Form() {
-	const nameRef = useRef<HTMLInputElement>(null)
-	const ageRef = useRef<HTMLInputElement>(null)
-	const person = { name: '', age: 0 }
+	const [person, setPerson] = useState({ name: '', age: '' })
 
 	function handleSubmit(event: FormEvent) {
 		event.preventDefault()
-		if (nameRef.current !== null) {
-			person.name = nameRef.current.value
-		}
-		if (ageRef.current !== null) {
-			person.age = parseInt(ageRef.current.value)
-		}
 		console.log(person)
 	}
 
@@ -22,13 +14,27 @@ function Form() {
 				<label htmlFor="name" className="form-label">
 					Name
 				</label>
-				<input ref={nameRef} id="name" type="text" className="form-control" />
+				<input
+					value={person.name}
+					onChange={event => setPerson({ ...person, name: event.target.value })}
+					id="name"
+					type="text"
+					className="form-control"
+				/>
 			</div>
 			<div className="mb-3">
 				<label htmlFor="age" className="form-label">
 					Age
 				</label>
-				<input ref={ageRef} id="age" type="number" className="form-control" />
+				<input
+					value={person.age}
+					onChange={event =>
+						setPerson({ ...person, age: parseInt(event.target.value) })
+					}
+					id="age"
+					type="number"
+					className="form-control"
+				/>
 			</div>
 			<button className="btn btn-primary" type="submit">
 				Submit
