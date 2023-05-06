41a1df494c121cb494dc1bbfa9043080624c72f5
Roller Lo <jasonbh02@gmail.com>
2023-05-06 14:43:39 +0800

    feat(Form): handling form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import ExpandableText from './components/ExpandableText'
+import Form from './components/Form'
 
 function App() {
 	return (
-		<div>
-			<ExpandableText>Hello World</ExpandableText>
+		<div className="p-4">
+			<Form />
 		</div>
 	)
 }
