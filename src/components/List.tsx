20eb05004d6e16e08a2c271c15948f953daba964
Roller Lo <jasonbh02@gmail.com>
2023-06-04 16:11:47 +0800

    feat: sum item amount & format total price

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,6 +11,12 @@ interface Props {
 }
 
 function List({ items }: Props) {
+	const total = items.reduce(
+		(total: number, item: Item) => item.amount + total,
+		0
+	)
+	const priceFormat = (price: number) => `$${price.toFixed(2)}`
+
 	return (
 		<table className="table table-bordered">
 			<thead>
@@ -25,11 +31,17 @@ function List({ items }: Props) {
 				{items.map(item => (
 					<tr key={item.description}>
 						<td>{item.description}</td>
-						<td>{item.amount}</td>
+						<td>{priceFormat(item.amount)}</td>
 						<td>{item.category}</td>
 						<td>Delete</td>
 					</tr>
 				))}
+				<tr>
+					<th>Total</th>
+					<td>{priceFormat(total)}</td>
+					<td></td>
+					<td></td>
+				</tr>
 			</tbody>
 		</table>
 	)
