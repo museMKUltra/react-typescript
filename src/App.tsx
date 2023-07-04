c757dd37a4805ad35de05415fdf5d452c4cf7c93
Roller Lo <jasonbh02@gmail.com>
2023-07-05 07:51:04 +0800

    feat(App): examples for using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from 'react'
 import './App.css'
 import ExpenseTracker from './expense-tracker/ExpenseTracker'
 
 function App() {
+	const ref = useRef<HTMLInputElement>(null)
+
+	// afterRender
+	useEffect(() => {
+		// side effect
+		if (ref.current) {
+			ref.current.focus()
+		}
+	})
+
+	useEffect(() => {
+		document.title = 'My App'
+	})
+
 	return (
 		<>
 			<ExpenseTracker />
+			<input ref={ref} type="text" className="form-control" />
 		</>
 	)
 }
