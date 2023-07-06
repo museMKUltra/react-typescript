a74370ae681eb5743016debfc63b320dda39eccb
Roller Lo <jasonbh02@gmail.com>
2023-07-06 08:01:25 +0800

    feat(App): add select to set category on change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
 import ProductList from './components/ProductList'
+import { useState } from 'react'
 
 function App() {
+	const [category, setCategory] = useState('')
+
 	return (
-		<>
+		<div className="p-4">
+			<select
+				className="form-select"
+				onChange={event => setCategory(event.target.value)}
+			>
+				<option value=""></option>
+				<option value="Clothing">Clothing</option>
+				<option value="Household">Household</option>
+			</select>
 			<ProductList />
-		</>
+		</div>
 	)
 }
 
