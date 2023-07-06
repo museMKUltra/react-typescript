e31bf7f68c114614eea124229337752a52834b89
Roller Lo <jasonbh02@gmail.com>
2023-07-07 07:58:16 +0800

    feat: get users with interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './App.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+interface User {
+	id: number
+	name: string
+}
+
 function App() {
 	const [users, setUsers] = useState([])
 
 	useEffect(() => {
 		axios
-			.get('https://jsonplaceholder.typicode.com/users')
-			.then(res => console.log(res))
+			.get<User[]>('https://jsonplaceholder.typicode.com/users')
+			.then(res => console.log(res.data[0].name))
 	})
 
 	return <div />
